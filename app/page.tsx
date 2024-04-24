86a509a2608ade49a13ab42c939fbe5fd7a1a484
morohoshikengo <morohoshi.kengo@kuwadatenoki.com>
2024-04-24 16:15:27 +0900

    これけしたらどうなる

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import { LoginButton } from "./(home)/@unauthenticated/LoginButton";
 
 export default function Home() {
-  return (
-    <>
-      <p>dddd</p>
-      <LoginButton />
-    </>
-  );
+  return <></>;
 }
